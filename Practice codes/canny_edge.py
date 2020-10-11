c0ee5a43d0e18ec93a41d55b7b796e1a91328d83
Naraharirahul <nmalaya@clemson.edu>
2020-10-11 02:00:52 -0400

    able to detect the lane's curvature

--- Practice codes/canny_edge.py
+++ Practice codes/canny_edge.py
@@ -7,7 +7,7 @@ import cv2
 # Read in the image and convert to grayscale
 image = mpimg.imread('exit-ramp.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-
+print(image.shape)
 # Define a kernel size for Gaussian smoothing / blurring
 # Note: this step is optional as cv2.Canny() applies a 5x5 Gaussian internally
 kernel_size = 3
