8cf19a4bd8167693ca7703e6f9534e47e04901bb
Naraharirahul <nmalaya@clemson.edu>
2020-10-23 22:58:39 -0400

    completed the visualization step

--- advance_lane_det/lane_det.py
+++ advance_lane_det/lane_det.py
@@ -71,6 +71,7 @@ def histogram(img):
     bottom_half = img[img.shape[0] //2 :,:]
     sum_hist = np.sum(bottom_half,axis=0)
     out_img = np.dstack((img, img, img))
+    window_img = np.zeros_like(out_img)
     midpoint = np.int(sum_hist.shape[0]//2)
     leftx_base = np.argmax(sum_hist[:midpoint])
     rightx_base = np.argmax(sum_hist[midpoint:]) + midpoint
@@ -93,13 +94,13 @@ def histogram(img):
     for window in range(nwindows):
         win_y_low = img.shape[0] - (window+1)*window_height
         win_y_high = img.shape[0] - (window)*window_height
-        win_x_left_low = leftx_base - margin
-        win_x_left_high = leftx_base + margin
-        win_x_right_low = rightx_base - margin
-        win_x_right_high = rightx_base + margin
+        win_x_left_low = leftx_current - margin
+        win_x_left_high = leftx_current + margin
+        win_x_right_low = rightx_current - margin
+        win_x_right_high = rightx_current + margin
 
-        cv2.rectangle(out_img,(win_x_left_low, win_y_low), (win_x_left_high, win_y_high), (0,255,0), 2)
-        cv2.rectangle(out_img, (win_x_right_low, win_y_low), (win_x_right_high, win_y_high), (0,255,0), 2)
+        # cv2.rectangle(out_img,(win_x_left_low, win_y_low), (win_x_left_high, win_y_high), (0,255,0), 2)
+        # cv2.rectangle(out_img, (win_x_right_low, win_y_low), (win_x_right_high, win_y_high), (0,255,0), 2)
 
         good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_x_left_low) & (nonzerox <= win_x_left_high)).nonzero()[0]
         good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_x_right_low) & (nonzerox <= win_x_right_high)).nonzero()[0]
@@ -107,8 +108,7 @@ def histogram(img):
 
         left_lane_inds.append(good_left_inds)
         right_lane_inds.append(good_right_inds)
-        print(left_lane_inds)
-        print(len(good_right_inds), 'right')
+
         if len(good_left_inds) > minipix:
             leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
         if(len(good_right_inds)) > minipix:
@@ -122,19 +122,14 @@ def histogram(img):
 
     leftx = nonzerox[left_lane_inds]
     lefty = nonzeroy[left_lane_inds]
-    print(leftx)
+
     rightx = nonzerox[right_lane_inds]
     righty = nonzeroy[right_lane_inds]
-    
-    return leftx, lefty, rightx, righty, out_img
-
-def polynomial_fit(img):
-    leftx, lefty, rightx, righty, out_img = histogram(img)
 
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
-
-    ploty = np.linspace(0, img.shape[0]-1, img.shape[0] )
+    # print(left_fit)
+    ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
     try:
         left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
         right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
@@ -153,17 +148,26 @@ def polynomial_fit(img):
     plt.plot(left_fitx, ploty, color='yellow')
     plt.plot(right_fitx, ploty, color='yellow')
 
-    return out_img
+    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, 
+                              ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
 
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+
+    return result
 
 files = os.listdir("camera_cal/")
 objectpoints, imagepoints = calibration(files)
 undist = cal_undist(objectpoints, imagepoints)
 warped, M_inv, M = perspective(undist)
 binary = threshold(warped)
-out_img = polynomial_fit(binary)
-# sum = histogram(binary)
-# plt.plot(sum)
+out_img = histogram(binary)
 plt.imshow(out_img, cmap='gray')
 plt.show()
 
